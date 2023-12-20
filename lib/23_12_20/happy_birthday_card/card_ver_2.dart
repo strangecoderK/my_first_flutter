6c930cd49f58e4e7907fa11e45564a4c8d411b4d
Kimdahee <dh980502@gmail.com>
2023-12-21 06:24:14 +0900

    생일축하카드 ver.2

---
+++ lib/23_12_20/happy_birthday_card/card_ver_2.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const BirthDayCard(),
+    );
+  }
+}
+
+class BirthDayCard extends StatefulWidget {
+  const BirthDayCard({super.key});
+
+  @override
+  State<BirthDayCard> createState() => _BirthDayCardState();
+}
+
+class _BirthDayCardState extends State<BirthDayCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.network(
+            'https://img.freepik.com/premium-photo/wallpapers-for-iphone-is-the-best-high-definition-iphone-wallpaper-in-you-can-make-this-wallpaper-for-your-iphone-x-backgrounds-mobile-screensaver-or-ipad-lock-screen-iphone-cute-cute_902639-30990.jpg',
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Happy\nBirthDay\nVicky',
+                  style: TextStyle(
+                    fontSize: 100,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  'from Chrity',
+                  style: TextStyle(fontSize: 30),
+                  textAlign: TextAlign.center,
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
