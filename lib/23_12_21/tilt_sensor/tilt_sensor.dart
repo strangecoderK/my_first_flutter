916903d7204e1bd3171dbc2a65706109fdf44d8a
Kimdahee <dh980502@gmail.com>
2023-12-22 08:13:22 +0900

    수평계 값 추가

--- lib/23_12_21/tilt_sensor/tilt_sensor.dart
+++ lib/23_12_21/tilt_sensor/tilt_sensor.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Domo',
+      title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.pink),
       home: const SensorApp(),
     );
@@ -22,22 +24,40 @@ class SensorApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      //DeviceOrientation.landscapeRight
+    ]);
     final centerX = MediaQuery.of(context).size.width / 2 - 100;
     final centerY = MediaQuery.of(context).size.height / 2 - 100;
-    return Scaffold(backgroundColor: Colors.pink[100],
+    return Scaffold(
+      backgroundColor: Colors.pink[100],
       body: Stack(
         children: [
-          Positioned(
-            left: centerX,
-            top: centerY,
-            child: Container(
-              child: Image.asset(
-                'assets/flower.png',
-                width: 200,
-                height: 200,
-              ),
-            ),
-          ),
+          StreamBuilder<AccelerometerEvent>(
+              stream: accelerometerEventStream(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                final event = snapshot.data!;
+                List<double> accelerometerValues = [event.x, event.y, event.z];
+                print(accelerometerValues);
+                return Positioned(
+                  left: centerX + event.y * 20,
+                  top: centerY + event.x * 20,
+                  child: Container(
+                    child: Image.asset(
+                      'assets/flower.png',
+                      width: 200,
+                      height: 200,
+                    ),
+                  ),
+                );
+              }),
         ],
       ),
     );
