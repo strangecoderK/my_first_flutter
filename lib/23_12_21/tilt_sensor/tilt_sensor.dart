3e55afc82df4e46fc6088174f309e524cc78b2ec
Kimdahee <dh980502@gmail.com>
2023-12-22 07:50:40 +0900

    수평계 이미지 변경

---
+++ lib/23_12_21/tilt_sensor/tilt_sensor.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Domo',
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: const SensorApp(),
+    );
+  }
+}
+
+class SensorApp extends StatelessWidget {
+  const SensorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final centerX = MediaQuery.of(context).size.width / 2 - 100;
+    final centerY = MediaQuery.of(context).size.height / 2 - 100;
+    return Scaffold(backgroundColor: Colors.pink[100],
+      body: Stack(
+        children: [
+          Positioned(
+            left: centerX,
+            top: centerY,
+            child: Container(
+              child: Image.asset(
+                'assets/flower.png',
+                width: 200,
+                height: 200,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
