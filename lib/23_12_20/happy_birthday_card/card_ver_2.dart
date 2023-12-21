4d37f63d677c00ec2c15c243f67aebc8ccf0f43a
Kimdahee <dh980502@gmail.com>
2023-12-21 16:04:15 +0900

    스텍에서 너비 맞추기

--- lib/23_12_20/happy_birthday_card/card_ver_2.dart
+++ lib/23_12_20/happy_birthday_card/card_ver_2.dart
@@ -32,25 +32,29 @@ class _BirthDayCardState extends State<BirthDayCard> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [
-          Image.network(
-            'https://img.freepik.com/premium-photo/wallpapers-for-iphone-is-the-best-high-definition-iphone-wallpaper-in-you-can-make-this-wallpaper-for-your-iphone-x-backgrounds-mobile-screensaver-or-ipad-lock-screen-iphone-cute-cute_902639-30990.jpg',
-            width: double.infinity,
-            height: double.infinity,
+
+        children: <Widget>[
+          Positioned.fill(
+            child: Image.network(
+              'https://img.freepik.com/premium-photo/wallpapers-for-iphone-is-the-best-high-definition-iphone-wallpaper-in-you-can-make-this-wallpaper-for-your-iphone-x-backgrounds-mobile-screensaver-or-ipad-lock-screen-iphone-cute-cute_902639-30990.jpg',
+              // width: double.infinity,
+              // height: double.infinity,
+              fit: BoxFit.cover,
+            ),
           ),
-          Center(
+          const Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  'Happy\nBirthDay\nVicky',
+                  'Happy\nBirthDay\nVicky!',
                   style: TextStyle(
                     fontSize: 100,
                   ),
                   textAlign: TextAlign.center,
                 ),
                 Text(
-                  'from Chrity',
+                  'from Christy',
                   style: TextStyle(fontSize: 30),
                   textAlign: TextAlign.center,
                 )
